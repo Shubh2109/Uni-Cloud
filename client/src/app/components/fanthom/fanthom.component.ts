dea729205e8a6150a0ebd6505c07c57b828d56b5
shubh_21 <shubham.sharma@mobisy.com>
2018-04-30 11:36:26 +0530

    Uploading Project

---
+++ client/src/app/components/fanthom/fanthom.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { DropboxService } from '../../services/dropbox.service';
+
+@Component({
+  selector: 'app-fanthom',
+  templateUrl: './fanthom.component.html',
+  styleUrls: ['./fanthom.component.css']
+})
+export class FanthomComponent implements OnInit {
+
+  constructor( private dropboxService : DropboxService) { }
+
+  ngOnInit() {
+    var code = window.location.href.split('?');
+    var token = code[1].split('=');
+    console.log("code = "+token[1]);
+    var user = JSON.parse(localStorage.getItem("user"));
+  //  console.log(user.id);
+    this.dropboxService.StoreDropboxToken(token[1],user.id);
+    location.assign("http://localhost:4200/dashboard");
+  }
+
+}
